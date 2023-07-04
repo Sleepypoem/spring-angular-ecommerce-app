4c877892d0345f643e9be3f996bca0ca4ad4f5cf
Alexander <gallade2014@gmail.com>
2023-07-04 15:28:39 -0600

    Modified product repository, added methods to get by category and to search by keyword.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/repositories/ProductRepository.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/repositories/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.sleepypoem.ecommerce.repositories;
 
 import com.sleepypoem.ecommerce.domain.entities.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "products", collectionResourceRel = "products")
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Page<ProductEntity> findAllByCategoryId(@Param("id") Long categoryId, Pageable pageable);
+
+    Page<ProductEntity> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
