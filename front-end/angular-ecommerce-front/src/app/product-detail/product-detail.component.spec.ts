cf1e4e41270d6b1a3b23a6c3101bd8dc1bf160dd
Alexander <gallade2014@gmail.com>
2023-07-07 16:27:48 -0600

    Fixed test, i will keep researching about unit test in Angular.

--- front-end/angular-ecommerce-front/src/app/product-detail/product-detail.component.spec.ts
+++ front-end/angular-ecommerce-front/src/app/product-detail/product-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDetailComponent } from './product-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
@@ -8,8 +10,9 @@ describe('ProductDetailComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductDetailComponent]
-    });
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [ProductDetailComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(ProductDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
