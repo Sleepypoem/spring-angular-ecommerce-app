74daf79d7b4b6b7bde8ab2667b5651f30e9ab984
Alexander <gallade2014@gmail.com>
2023-07-17 10:37:21 -0600

    Added okta configurations and dependencies.

---
+++ front-end/angular-ecommerce-front/auth-server-config.ts
@@ -0,0 +1,9 @@
+export default {
+  oidc: {
+    clientId: '0oa2x8hqh4uq0n8Xr5d7',
+    issuer: 'https://dev-80197349.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+  },
+};
