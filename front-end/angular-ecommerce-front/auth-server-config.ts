63ea32a65693ca0ec804d724132e04fccb6f9b53
Anibal Alexander García Blanco <101472659+Sleepypoem@users.noreply.github.com>
2023-07-18 16:50:16 -0600

    Delete auth-server-config.ts
    Common junior mistake

--- front-end/angular-ecommerce-front/auth-server-config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  oidc: {
-    clientId: '0oa2x8hqh4uq0n8Xr5d7',
-    issuer: 'https://dev-80197349.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['openid', 'profile', 'email'],
-    pkce: true,
-  },
-};
