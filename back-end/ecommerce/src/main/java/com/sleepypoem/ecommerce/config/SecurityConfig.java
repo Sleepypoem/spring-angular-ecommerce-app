d92eba73a2a9a9460fe11237870bea48b6816c02
Alexander <gallade2014@gmail.com>
2023-07-23 00:58:51 -0600

    Modified SecurityConfig, secured checkout and orders endpoints.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/SecurityConfig.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
@@ -14,14 +15,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+        http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/orders/**").authenticated()
+                        .requestMatchers("/checkout/*").authenticated()
+                        .anyRequest().permitAll()
+                )
                 .oauth2Login(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
 
         //This line tells the client which authentication scheme to use.
         http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
 
         Okta.configureResourceServer401ResponseBody(http);
+        http.cors(Customizer.withDefaults());
 
         return http.build();
     }
