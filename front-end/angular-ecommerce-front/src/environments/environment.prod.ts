02ba889df1f78ae8d2bfe930b7723fa027c2e635
Alexander <gallade2014@gmail.com>
2023-07-25 22:43:26 -0600

    Added environment variables to the app.

---
+++ front-end/angular-ecommerce-front/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  backendUrl: 'https://localhost:8081/',
+  securedEndpoints: [
+    'https://localhost:8081/orders',
+    'https://localhost:8081/checkout',
+  ],
+};
---
+++ front-end/angular-ecommerce-front/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  backendUrl: 'https://localhost:8081/',
+  securedEndpoints: [
+    'https://localhost:8081/orders',
+    'https://localhost:8081/checkout',
+  ],
+};
