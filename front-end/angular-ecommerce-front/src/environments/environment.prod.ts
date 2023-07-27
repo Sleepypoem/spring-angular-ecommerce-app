5733a1ee7ef82751bec451e074867004e0c46dae
Alexander <gallade2014@gmail.com>
2023-07-27 13:57:29 -0600

    Fixed a bug when trying to checkout with a non logged customer.

--- front-end/angular-ecommerce-front/src/environments/environment.prod.ts
+++ front-end/angular-ecommerce-front/src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
   backendUrl: 'https://localhost:8081/',
-  securedEndpoints: [
-    'https://localhost:8081/orders',
-    'https://localhost:8081/checkout',
-  ],
+  securedEndpoints: ['https://localhost:8081/orders'],
 };
