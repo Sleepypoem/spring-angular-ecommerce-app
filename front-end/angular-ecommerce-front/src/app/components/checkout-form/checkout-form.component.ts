5388fcb46b023886f9ae6df2c640515ab942aafd
Alexander <gallade2014@gmail.com>
2023-07-27 14:06:03 -0600

    Refactorized checkout form component, now is more legible and each section is on its own component.

--- front-end/angular-ecommerce-front/src/app/components/checkout-form/checkout-form.component.ts
+++ front-end/angular-ecommerce-front/src/app/components/checkout-form/checkout-form.component.ts
@@ -1,21 +1,17 @@
 import { Customer } from './../../dtos/customer';
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ValidationErrors,
-  Validators,
-} from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ICountry, IState } from 'country-state-city';
 import { Address } from 'src/app/dtos/address';
 import { CreditCard } from 'src/app/dtos/creditCard';
 import { Order } from 'src/app/dtos/order';
 import { Purchase } from 'src/app/dtos/purchase';
-import { AddressService } from 'src/app/services/address.service';
 import { CartService } from 'src/app/services/cart.service';
 import { CheckoutService } from 'src/app/services/checkout.service';
-import { CustomValidators } from 'src/app/validators/CustomValidators';
+import { CustomerFormComponent } from '../customer-form/customer-form.component';
+import { AddressFormComponent } from '../address-form/address-form.component';
+import { CreditCardFormComponent } from '../credit-card-form/credit-card-form.component';
+import { BillingAddressFormComponent } from '../billing-address-form/billing-address-form.component';
 
 @Component({
   selector: 'app-checkout-form',
@@ -23,10 +19,19 @@ import { CustomValidators } from 'src/app/validators/CustomValidators';
   styleUrls: ['./checkout-form.component.css'],
 })
 export class CheckoutFormComponent implements OnInit {
-  checkoutForm: FormGroup = {} as FormGroup;
-  countries: ICountry[] = [];
+  @ViewChild(CustomerFormComponent, { static: true })
+  public customerFormComponent: CustomerFormComponent;
 
-  states: IState[] = [];
+  @ViewChild(AddressFormComponent, { static: true })
+  public addressFormComponent: AddressFormComponent;
+
+  @ViewChild(BillingAddressFormComponent, { static: true })
+  public billingAddressFormComponent: BillingAddressFormComponent;
+
+  @ViewChild(CreditCardFormComponent, { static: true })
+  public creditCardFormComponent: CreditCardFormComponent;
+
+  checkoutForm: FormGroup;
 
   totalPrice: number = 0.0;
 
@@ -36,32 +41,15 @@ export class CheckoutFormComponent implements OnInit {
 
   sessionStorage: Storage = sessionStorage;
 
-  customer: Customer = new Customer();
-
-  todayDate: Date = new Date();
-
-  actualMonth: number = this.todayDate.getMonth() + 1;
-
-  actualYear: number = this.todayDate.getFullYear();
-
-  commonValidators: ValidationErrors[] = [
-    Validators.required,
-    CustomValidators.notOnlyBlankSpaces,
-  ];
+  customer: Customer;
 
   constructor(
     private formBuilder: FormBuilder,
-    private addressService: AddressService,
     private cartService: CartService,
     private checkouService: CheckoutService,
     private router: Router
   ) {
-    this.countries = this.addressService.getCountries();
     this.customer = JSON.parse(this.sessionStorage.getItem('customer')!);
-  }
-
-  createField(initialValue: string, validators: ValidationErrors[] = []): any {
-    return [initialValue, validators];
   }
 
   ngOnInit() {
@@ -75,20 +63,16 @@ export class CheckoutFormComponent implements OnInit {
 
   private buildForm(): void {
     this.checkoutForm = this.formBuilder.group({
-      customer: this.formBuilder.group(this.createCustomerFields()),
-      address: this.formBuilder.group(this.createAddressFields()),
+      customer: this.customerFormComponent.buildForm(),
+      address: this.addressFormComponent.buildForm(),
 
-      billingAddress: this.formBuilder.group(this.createAddressFields()),
-      creditCard: this.formBuilder.group(this.createCreditCardFields()),
+      billingAddress: this.billingAddressFormComponent.buildForm(),
+      creditCard: this.creditCardFormComponent.buildForm(),
     });
   }
 
   private hideCustomerSection(): void {
     this.checkoutForm.controls['customer'].disable();
-  }
-
-  loadCountryStatesByCountryName(country: any) {
-    this.states = this.addressService.getStatesByCountry(country);
   }
 
   updateCartReviewDetails() {
@@ -98,105 +82,24 @@ export class CheckoutFormComponent implements OnInit {
     );
   }
 
-  private createCustomerFields(): any {
-    return {
-      firstName: this.createField(
-        this.customer?.firstName!,
-        this.commonValidators.concat(
-          Validators.minLength(3),
-          Validators.maxLength(50)
-        )
-      ),
-      lastName: this.createField(
-        this.customer?.lastName!,
-        this.commonValidators.concat(
-          Validators.minLength(3),
-          Validators.maxLength(50)
-        )
-      ),
-      email: this.createField(this.customer?.email!, [
-        Validators.required,
-        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
-      ]),
-    };
-  }
-
-  private createAddressFields(): any {
-    return {
-      country: this.createField('Select a country...', this.commonValidators),
-      street: this.createField(
-        '',
-        this.commonValidators.concat(
-          Validators.minLength(5),
-          Validators.maxLength(50)
-        )
-      ),
-      city: this.createField(
-        '',
-        this.commonValidators.concat(Validators.maxLength(50))
-      ),
-      state: this.createField('Select a state...', this.commonValidators),
-      zipCode: this.createField('', this.commonValidators),
-    };
-  }
-
-  private createCreditCardFields(): any {
-    let today = new Date();
-    return {
-      cardNumber: this.createField(
-        '',
-        this.commonValidators.concat(
-          Validators.maxLength(16),
-          Validators.minLength(16),
-          Validators.pattern(/^\d+$/)
-        )
-      ),
-      cardHolderName: this.createField(
-        '',
-        this.commonValidators.concat(
-          Validators.minLength(3),
-          Validators.maxLength(50)
-        )
-      ),
-      cardExpirationMonth: this.createField(
-        today.getMonth() + 1 + '',
-        this.commonValidators.concat(
-          Validators.maxLength(2),
-          Validators.minLength(2),
-          Validators.pattern(/^\d+$/)
-        )
-      ),
-      cardExpirationYear: this.createField(
-        today.getFullYear() + '',
-        this.commonValidators.concat(
-          Validators.maxLength(4),
-          Validators.minLength(4),
-          Validators.pattern(/^\d+$/)
-        )
-      ),
-      cardSecurityCode: this.createField(
-        '',
-        this.commonValidators.concat(Validators.maxLength(3))
-      ),
-      cardType: this.createField('', this.commonValidators),
-    };
-  }
-
   copyAddressToBillingAddress(event: any) {
     if (event.target.checked) {
-      this.checkoutForm.controls['billingAddress'].setValue(
-        this.checkoutForm.get('address')?.value
+      this.billingAddressFormComponent.addressForm.setValue(
+        this.addressFormComponent.addressForm.value
       );
     } else {
-      this.checkoutForm.controls['billingAddress'].reset();
+      this.billingAddressFormComponent.resetValues();
     }
   }
 
   onSubmit() {
     if (this.checkoutForm.invalid) {
       this.markFieldsAndDisplayErrors();
+      console.log(this.checkoutForm.value);
       return;
     }
+    console.log(this.checkoutForm.value);
+
     let purchase = this.preparePurchase();
     this.makeOrder(purchase);
   }
@@ -253,112 +156,40 @@ export class CheckoutFormComponent implements OnInit {
 
   public getCustomerFromForm(): Customer {
     return new Customer()
-      .withFirstName(this.firstName?.value)
-      .withLastName(this.lastName?.value)
-      .withEmail(this.email?.value);
+      .withFirstName(this.customerFormComponent.firstName?.value)
+      .withLastName(this.customerFormComponent.lastName?.value)
+      .withEmail(this.customerFormComponent.email?.value);
   }
 
   public getBillingAddressFromForm(): Address {
     return new Address()
-      .withCity(this.billingCity?.value)
-      .withCountry(this.billingCountry?.value)
-      .withState(this.billingState?.value)
-      .withStreet(this.billingStreet?.value)
-      .withZipCode(this.billingZipCode?.value);
+      .withCity(this.billingAddressFormComponent.city?.value)
+      .withCountry(this.billingAddressFormComponent.country?.value)
+      .withState(this.billingAddressFormComponent.state?.value)
+      .withStreet(this.billingAddressFormComponent.street?.value)
+      .withZipCode(this.billingAddressFormComponent.zipCode?.value);
   }
 
   public getShippingAddressFromForm(): Address {
     return new Address()
-      .withCity(this.city?.value)
-      .withCountry(this.country?.value)
-      .withState(this.state?.value)
-      .withStreet(this.street?.value)
-      .withZipCode(this.zipCode?.value);
+      .withCity(this.addressFormComponent.city?.value)
+      .withCountry(this.addressFormComponent.country?.value)
+      .withState(this.addressFormComponent.state?.value)
+      .withStreet(this.addressFormComponent.street?.value)
+      .withZipCode(this.addressFormComponent.zipCode?.value);
   }
 
   public getCreditCardFromForm(): CreditCard {
     return new CreditCard()
-      .withCardHolderName(this.cardHolderName?.value)
-      .withCardNumber(this.cardNumber?.value)
-      .withExpirationMonth(this.cardExpirationMonth?.value)
-      .withExpirationYear(this.cardExpirationYear?.value)
-      .withSecurityCode(this.cardSecurityCode?.value)
-      .withCardType(this.cardType?.value);
-  }
-
-  public get firstName() {
-    return this.checkoutForm.get('customer.firstName');
-  }
-
-  public get lastName() {
-    return this.checkoutForm.get('customer.lastName');
-  }
-
-  public get email() {
-    return this.checkoutForm.get('customer.email');
-  }
-
-  public get street() {
-    return this.checkoutForm.get('address.street');
-  }
-
-  public get city() {
-    return this.checkoutForm.get('address.city');
-  }
-
-  public get state() {
-    return this.checkoutForm.get('address.state');
-  }
-
-  public get zipCode() {
-    return this.checkoutForm.get('address.zipCode');
-  }
-
-  public get country() {
-    return this.checkoutForm.get('address.country');
-  }
-
-  public get billingStreet() {
-    return this.checkoutForm.get('billingAddress.street');
-  }
-
-  public get billingCity() {
-    return this.checkoutForm.get('billingAddress.city');
-  }
-
-  public get billingState() {
-    return this.checkoutForm.get('billingAddress.state');
-  }
-
-  public get billingZipCode() {
-    return this.checkoutForm.get('billingAddress.zipCode');
-  }
-
-  public get billingCountry() {
-    return this.checkoutForm.get('billingAddress.country');
-  }
-
-  public get cardNumber() {
-    return this.checkoutForm.get('creditCard.cardNumber');
-  }
-
-  public get cardHolderName() {
-    return this.checkoutForm.get('creditCard.cardHolderName');
-  }
-
-  public get cardExpirationMonth() {
-    return this.checkoutForm.get('creditCard.cardExpirationMonth');
-  }
-
-  public get cardExpirationYear() {
-    return this.checkoutForm.get('creditCard.cardExpirationYear');
-  }
-
-  public get cardSecurityCode() {
-    return this.checkoutForm.get('creditCard.cardSecurityCode');
-  }
-
-  public get cardType() {
-    return this.checkoutForm.get('creditCard.cardType');
+      .withCardHolderName(this.creditCardFormComponent.cardHolderName?.value)
+      .withCardNumber(this.creditCardFormComponent.cardNumber?.value)
+      .withExpirationMonth(
+        this.creditCardFormComponent.cardExpirationMonth?.value
+      )
+      .withExpirationYear(
+        this.creditCardFormComponent.cardExpirationYear?.value
+      )
+      .withSecurityCode(this.creditCardFormComponent.cardSecurityCode?.value)
+      .withCardType(this.creditCardFormComponent.cardType?.value);
   }
 }
