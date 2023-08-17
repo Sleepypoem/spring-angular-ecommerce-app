acc0c0045eb575faf3435c011f0d7e48dc4c0d89
Alexander <gallade2014@gmail.com>
2023-08-17 15:01:44 -0600

    Modified security config file to protect okta users endpoint and enable method security.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/SecurityConfig.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/SecurityConfig.java
@@ -1,22 +1,28 @@
 package com.sleepypoem.ecommerce.config;
 
 import com.okta.spring.boot.oauth.Okta;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
+@EnableWebSecurity
+@Slf4j
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
                         .requestMatchers("/orders/**").authenticated()
+                        .requestMatchers("/okta/users/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .oauth2Login(Customizer.withDefaults())
@@ -31,4 +37,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
