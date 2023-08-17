fb5eaa8f8c1bcc9a78bbeef11a893043514810db
Alexander <gallade2014@gmail.com>
2023-08-17 14:55:22 -0600

    Created Customer service.

---
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/service/interfaces/CustomerService.java
@@ -0,0 +1,30 @@
+package com.sleepypoem.ecommerce.service.interfaces;
+
+import com.sleepypoem.ecommerce.domain.entities.CustomerEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService {
+
+    CustomerEntity create(CustomerEntity customerEntity);
+
+    CustomerEntity getById(Long id);
+
+    CustomerEntity getByEmail(String email);
+
+    CustomerEntity update(Long id, CustomerEntity customerEntity);
+
+    void deleteById(Long id);
+
+    Page<CustomerEntity> getAll(Pageable pageable);
+
+
+    Page<CustomerEntity> getAllByFirstNameContaining(String firstName, Pageable pageable);
+
+
+    Page<CustomerEntity> getAllByLastNameContaining(String lastName, Pageable pageable);
+
+    boolean existsByEmail(String email);
+
+    boolean existsById(Long id);
+}
