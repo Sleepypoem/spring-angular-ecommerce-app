ad5a65977a57abe128cc85f6d67cb490ceebdad8
Alexander <gallade2014@gmail.com>
2023-08-17 14:53:10 -0600

    Modified Customer Repository annotation to implement custom service logic.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/repositories/CustomerRepository.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/repositories/CustomerRepository.java
@@ -4,11 +4,11 @@ import com.sleepypoem.ecommerce.domain.entities.CustomerEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@RepositoryRestResource(path = "customers", collectionResourceRel = "customers")
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
     Optional<CustomerEntity> findByEmail(String email);
