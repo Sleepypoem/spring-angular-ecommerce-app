950a3acd8cba08ca7bb61fabe5160840b37c6d30
Alexander <gallade2014@gmail.com>
2023-08-17 15:03:02 -0600

    Created okta users endpoint and secured it with preauthorize.

---
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/controllers/OktaUserController.java
@@ -0,0 +1,41 @@
+package com.sleepypoem.ecommerce.controllers;
+
+import com.okta.sdk.resource.model.User;
+import com.sleepypoem.ecommerce.domain.entities.CustomerEntity;
+import com.sleepypoem.ecommerce.service.interfaces.OktaUserService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/okta/users")
+@PreAuthorize("authentication.token.claims['role'] == 'admin'")
+public class OktaUserController {
+
+    private final OktaUserService oktaUserService;
+
+
+    public OktaUserController(OktaUserService oktaUserService) {
+        this.oktaUserService = oktaUserService;
+    }
+
+    @PostMapping
+    public void createOktaUser(@RequestBody CustomerEntity customerEntity) {
+        oktaUserService.createOktaUser(customerEntity);
+    }
+
+    @GetMapping("/{id}")
+    public User getOktaUserById(@PathVariable String id) {
+        return oktaUserService.getOktaUser(id);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteOktaUserById(@PathVariable String id) {
+        oktaUserService.deleteOktaUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateOktaUserById(@PathVariable String id, @RequestBody CustomerEntity customerEntity) {
+        oktaUserService.updateOktaUser(id, customerEntity);
+    }
+}
