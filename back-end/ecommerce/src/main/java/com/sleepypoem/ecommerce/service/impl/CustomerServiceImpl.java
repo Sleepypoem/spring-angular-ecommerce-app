db32f86259757d4be76b2fd89951863acec48d26
Alexander <gallade2014@gmail.com>
2023-08-17 21:48:10 -0600

    Added Customer service implementation.

---
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,104 @@
+package com.sleepypoem.ecommerce.service.impl;
+
+import com.okta.sdk.resource.client.ApiException;
+import com.sleepypoem.ecommerce.domain.entities.CustomerEntity;
+import com.sleepypoem.ecommerce.exceptions.MyEntityNotFoundException;
+import com.sleepypoem.ecommerce.repositories.CustomerRepository;
+import com.sleepypoem.ecommerce.service.interfaces.CustomerService;
+import com.sleepypoem.ecommerce.service.interfaces.ImageService;
+import com.sleepypoem.ecommerce.service.interfaces.OktaUserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    private final ImageService imageService;
+
+    private final OktaUserService oktaUserService;
+
+    private static final String IMAGE_FOLDER = "spring-angular-ecommerce/assets/images/customers/";
+
+    public CustomerServiceImpl(CustomerRepository customerRepository, ImageService imageService, OktaUserService oktaUserService) {
+        this.customerRepository = customerRepository;
+        this.imageService = imageService;
+        this.oktaUserService = oktaUserService;
+    }
+    @Override
+    @Transactional(rollbackFor = ApiException.class)
+    public CustomerEntity create(CustomerEntity customerEntity) {
+        if(customerEntity.getEncodedImage() == null){
+            customerEntity.setImage(imageService.getPlaceholderImage());
+        }else{
+            customerEntity.setImage(imageService.addImageToFolder(customerEntity.getEncodedImage(), IMAGE_FOLDER));
+        }
+        oktaUserService.createOktaUser(customerEntity);
+        return customerRepository.save(customerEntity);
+    }
+
+    @Override
+    public CustomerEntity getById(Long id) {
+        return customerRepository.findById(id).orElseThrow(() -> new MyEntityNotFoundException("Customer not found with id: " + id));
+    }
+
+    @Override
+    public CustomerEntity getByEmail(String email) {
+        return customerRepository.findByEmail(email).orElseThrow(() -> new MyEntityNotFoundException("Customer not found with email: " + email));
+    }
+
+    @Override
+    @Transactional
+    public CustomerEntity update(Long id, CustomerEntity customerEntity) {
+        if(!Objects.equals(id, customerEntity.getId())){
+            throw new RuntimeException("Id: " + customerEntity.getId() + " not match with customer id: " + id);
+        }
+        CustomerEntity customerEntityFromDB = getById(id);
+        if(customerEntity.getEncodedImage() != null){
+            customerEntity.setImage(imageService.addImageToFolder(customerEntity.getEncodedImage(), IMAGE_FOLDER));
+            imageService.deleteImage(customerEntity.getImage());
+        }
+        oktaUserService.updateOktaUser(customerEntityFromDB.getEmail(), customerEntity);
+        //Email cannot be edited
+        customerEntity.setEmail(customerEntityFromDB.getEmail());
+        return customerRepository.save(customerEntity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        CustomerEntity customerEntity = getById(id);
+        imageService.deleteImage(customerEntity.getImage());
+        customerRepository.delete(customerEntity);
+        oktaUserService.deleteOktaUser(customerEntity.getEmail());
+    }
+
+    @Override
+    public Page<CustomerEntity> getAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<CustomerEntity> getAllByFirstNameContaining(String firstName, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContainingIgnoreCase(firstName, pageable);
+    }
+
+    @Override
+    public Page<CustomerEntity> getAllByLastNameContaining(String lastName, Pageable pageable) {
+        return customerRepository.findAllByLastNameContainingIgnoreCase(lastName, pageable);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return customerRepository.findByEmail(email).isPresent();
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return customerRepository.existsById(id);
+    }
+}
