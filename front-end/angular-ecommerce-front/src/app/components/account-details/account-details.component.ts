7fe84072038772844d6f125824be7b290b715685
Alexander <gallade2014@gmail.com>
2023-08-18 01:13:16 -0600

    Modified acount details component, it now shows the user image.

--- front-end/angular-ecommerce-front/src/app/components/account-details/account-details.component.ts
+++ front-end/angular-ecommerce-front/src/app/components/account-details/account-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Customer } from 'src/app/dtos/customer';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-account-details',
@@ -8,6 +9,7 @@ import { Customer } from 'src/app/dtos/customer';
 })
 export class AccountDetailsComponent {
   customer: Customer;
+  imageServerUrl: string = environment.imageServerUrl;
 
   storage: Storage = sessionStorage;
 
