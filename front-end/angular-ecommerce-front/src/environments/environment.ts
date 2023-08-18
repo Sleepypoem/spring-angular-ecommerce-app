f19b12b3862aec3b2b265cb850c15b50fcad4280
Alexander <gallade2014@gmail.com>
2023-08-18 01:11:44 -0600

    Added image server url to environment.

--- front-end/angular-ecommerce-front/src/environments/environment.ts
+++ front-end/angular-ecommerce-front/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   backendUrl: 'https://localhost:8081/',
   securedEndpoints: ['https://localhost:8081/orders'],
+  imageServerUrl: 'https://res.cloudinary.com/dazl3qqcm/image/upload/',
 };
