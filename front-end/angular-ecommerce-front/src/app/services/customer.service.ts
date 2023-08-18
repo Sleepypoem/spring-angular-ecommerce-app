b8ea4116f0dac1f2f5cfaf17493eab1843d4e5e7
Alexander <gallade2014@gmail.com>
2023-08-18 01:49:08 -0600

    Created a Form creation service which creates a Form based on an array of Fields.

---
+++ front-end/angular-ecommerce-front/src/app/services/customer.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Customer } from '../dtos/customer';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerService {
+  private baseUrl = environment.backendUrl + 'customers';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public getCustomerById(id: string): Observable<Customer> {
+    return this.httpClient.get<Customer>(this.baseUrl + '/' + id);
+  }
+
+  public getCustomerByEmail(email: string): Observable<Customer> {
+    return this.httpClient.get<Customer>(
+      this.baseUrl + '/search/findByEmail?email=' + email
+    );
+  }
+
+  public addCustomer(customer: Customer): Observable<Customer> {
+    return this.httpClient.post<Customer>(this.baseUrl, customer);
+  }
+
+  public updateCustomer(
+    customerId: number,
+    customer: Customer
+  ): Observable<Customer> {
+    return this.httpClient.put<Customer>(
+      this.baseUrl + '/' + customerId,
+      customer
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Customer[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
+}
---
+++ front-end/angular-ecommerce-front/src/app/services/form-creator.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FieldBase } from '../dtos/fieldBase';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormCreatorService {
+  toFormGroup(fields: FieldBase<string>[]) {
+    const group: any = {};
+
+    fields.forEach((field) => {
+      group[field.key] = new FormControl(
+        field.value || '',
+        field.validators || []
+      );
+    });
+    return new FormGroup(group);
+  }
+}
