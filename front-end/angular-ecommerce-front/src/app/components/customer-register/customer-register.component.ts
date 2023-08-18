b521b861bee3910b36c903efea70f97148fbbf68
Alexander <gallade2014@gmail.com>
2023-08-18 13:39:55 -0600

    Created a customer register component.

---
+++ front-end/angular-ecommerce-front/src/app/components/customer-register/customer-register.component.ts
@@ -0,0 +1,72 @@
+import { Router } from '@angular/router';
+import { CustomerService } from 'src/app/services/customer.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { customerSignUpForm } from 'src/config/forms';
+import { Customer } from 'src/app/dtos/customer';
+
+@Component({
+  selector: 'app-customer-register',
+  templateUrl: './customer-register.component.html',
+  styleUrls: ['./customer-register.component.css'],
+})
+export class CustomerRegisterComponent {
+  customerForm: any = customerSignUpForm;
+  formGroup: FormGroup;
+  image: string;
+
+  constructor(
+    private customerService: CustomerService,
+    private router: Router
+  ) {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.formGroup = new FormGroup({});
+  }
+
+  onSubmit() {
+    this.markAllSubformsAsTouched(this.formGroup);
+    this.formGroup.updateValueAndValidity();
+    let customer = this.customerFromForm;
+    customer.encodedImage = this.image;
+    this.makeRequest(customer);
+  }
+
+  makeRequest(customer: Customer) {
+    this.customerService.addCustomer(customer).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(['/customer/login']);
+      },
+      (error) => {
+        alert('Registration failed');
+        console.log(error);
+      }
+    );
+  }
+
+  private markAllSubformsAsTouched(formGroup: any) {
+    if (formGroup.controls == null) {
+      return;
+    }
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormGroup) {
+        this.markAllSubformsAsTouched(control);
+      } else if (control instanceof FormControl) {
+        control.markAsTouched();
+        control.markAsDirty();
+      }
+    });
+  }
+
+  receiveImage(event: any) {
+    this.image = event;
+  }
+
+  get customerFromForm(): Customer {
+    return this.formGroup.get('customer')?.value!;
+  }
+}
