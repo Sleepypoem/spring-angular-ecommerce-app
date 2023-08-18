d0af1bdcb3df656fcac1e16cfd72e2cec4e5e283
Alexander <gallade2014@gmail.com>
2023-08-18 01:33:07 -0600

    Added new properties to customer DTO.

--- front-end/angular-ecommerce-front/src/app/dtos/customer.ts
+++ front-end/angular-ecommerce-front/src/app/dtos/customer.ts
@@ -2,9 +2,11 @@ export class Customer {
   firstName?: string;
   lastName?: string;
   email?: string;
+  password?: string;
   phone?: string;
   image?: string;
   id?: number;
+  encodedImage?: string;
   constructor() {}
 
   withFirstName(firstName: string): Customer {
@@ -24,6 +26,26 @@ export class Customer {
 
   withId(id: number): Customer {
     this.id = id;
+    return this;
+  }
+
+  withPassword(password: string): Customer {
+    this.password = password;
+    return this;
+  }
+
+  withPhone(phone: string): Customer {
+    this.phone = phone;
+    return this;
+  }
+
+  withImage(image: string): Customer {
+    this.image = image;
+    return this;
+  }
+
+  withEncodedImage(encodedImage: string): Customer {
+    this.encodedImage = encodedImage;
     return this;
   }
 }
