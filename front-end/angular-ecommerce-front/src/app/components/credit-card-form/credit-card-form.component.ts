f9755f5df2d8f5dbcdbd2688600d91fa3dc98b4c
Alexander <gallade2014@gmail.com>
2023-08-18 11:33:33 -0600

    Modified checkout subforms components to use the new form logic.

--- front-end/angular-ecommerce-front/src/app/components/credit-card-form/credit-card-form.component.ts
+++ front-end/angular-ecommerce-front/src/app/components/credit-card-form/credit-card-form.component.ts
@@ -1,90 +1,72 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SubForm } from 'src/app/interfaces/SubForm';
-import { CustomValidators } from 'src/app/validators/CustomValidators';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { creditCardForm } from 'src/config/forms';
 
 @Component({
   selector: 'app-credit-card-form',
   templateUrl: './credit-card-form.component.html',
   styleUrls: ['./../forms-shared-css/forms.css'],
 })
-export class CreditCardFormComponent implements SubForm {
-  @Input()
-  parentForm: FormGroup;
-  @Input()
-  formGroupName: string;
-  creditCard: FormGroup;
+export class CreditCardFormComponent {
+  @Input() formGroup: FormGroup;
+  cardForm: any = creditCardForm;
   today: Date = new Date();
+  currentMonth = this.today.getMonth() + 1;
+  currentYear = this.today.getFullYear();
+  constructor() {
+    this.populateOptionsAndSetDefaults();
+  }
 
-  constructor(private formBuilder: FormBuilder) {}
-  buildForm(): FormGroup {
-    this.creditCard = this.formBuilder.group({
-      cardNumber: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(16),
-          Validators.minLength(16),
-          Validators.pattern(/^\d+$/),
-          CustomValidators.notOnlyBlankSpaces,
-        ],
-      ],
-      cardHolderName: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(50),
-          CustomValidators.notOnlyBlankSpaces,
-        ],
-      ],
-      cardExpirationMonth: [this.today.getMonth() + 1, []],
-      cardExpirationYear: [this.today.getFullYear(), []],
-      cardSecurityCode: [
-        '',
-        [
-          Validators.required,
-          Validators.maxLength(3),
-          Validators.minLength(3),
-          Validators.pattern(/^\d+$/),
-          CustomValidators.notOnlyBlankSpaces,
-        ],
-      ],
-      cardType: ['VISA', []],
-    });
-    return this.creditCard;
+  private createMonthOptions() {
+    let monthOptions = [];
+    for (let i = 1; i <= 12; i++) {
+      monthOptions.push({
+        key: i,
+        value: i,
+      });
+    }
+
+    return monthOptions;
+  }
+
+  private populateOptionsAndSetDefaults() {
+    this.cardForm[2].options = this.createMonthOptions();
+    this.cardForm[2].value = this.currentMonth;
+    this.cardForm[3].options = this.createYearOptions();
+    this.cardForm[3].value = this.currentYear;
+  }
+
+  private createYearOptions() {
+    let yearOptions = [];
+    for (let i = this.currentYear; i <= this.currentYear + 10; i++) {
+      yearOptions.push({
+        key: i,
+        value: i,
+      });
+    }
+    return yearOptions;
   }
 
   ngOnInit(): void {
-    this.creditCard
-      .get('cardExpirationMonth')
-      ?.setValue(this.today.getMonth() + 1);
-    this.creditCard
-      .get('cardExpirationYear')
-      ?.setValue(this.today.getFullYear());
+    this.formGroup?.valueChanges.subscribe((value) => {
+      if (value.creditCard != null) {
+        this.limitFieldInput(this.cvvField!, 3);
+        this.limitFieldInput(this.cardNumberField!, 16);
+      }
+    });
   }
 
-  public get cardNumber() {
-    return this.creditCard.get('cardNumber');
+  private limitFieldInput(field: AbstractControl, limit: number) {
+    if (field.value.length > limit) {
+      field.setValue(field.value.slice(0, -1));
+    }
   }
 
-  public get cardHolderName() {
-    return this.creditCard.get('cardHolderName');
+  get cvvField(): AbstractControl {
+    return this.formGroup.get('creditCard')?.get('cvv')!;
   }
 
-  public get cardExpirationMonth() {
-    return this.creditCard.get('cardExpirationMonth');
-  }
-
-  public get cardExpirationYear() {
-    return this.creditCard.get('cardExpirationYear');
-  }
-
-  public get cardSecurityCode() {
-    return this.creditCard.get('cardSecurityCode');
-  }
-
-  public get cardType() {
-    return this.creditCard.get('cardType');
+  get cardNumberField(): AbstractControl {
+    return this.formGroup.get('creditCard')?.get('cardNumber')!;
   }
 }
