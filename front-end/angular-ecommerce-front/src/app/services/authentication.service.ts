693f43ce24a65bd2d4cf3918899b6f77e36f5374
Alexander <gallade2014@gmail.com>
2023-08-20 01:27:57 -0600

    Created own authentication service to keep track of the logged user and his details.

---
+++ front-end/angular-ecommerce-front/src/app/services/authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Customer } from '../dtos/customer';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private loggedInUserSubject: BehaviorSubject<Customer | null> =
+    new BehaviorSubject<Customer | null>(null);
+
+  constructor() {}
+
+  login(user: Customer) {
+    this.loggedInUserSubject.next(user);
+  }
+
+  updateUser(updatedUser: Customer) {
+    this.loggedInUserSubject.next(updatedUser);
+  }
+
+  get loggedInUser$(): Observable<Customer | null> {
+    return this.loggedInUserSubject.asObservable();
+  }
+}
