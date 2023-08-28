ce632b5f57e6df567d941f2ea8ff608a9d4c05ca
Alexander <gallade2014@gmail.com>
2023-08-28 13:48:57 -0600

    Modified credit card form component to use stripe card element instead.

--- front-end/angular-ecommerce-front/src/app/components/credit-card-form/credit-card-form.component.ts
+++ front-end/angular-ecommerce-front/src/app/components/credit-card-form/credit-card-form.component.ts
@@ -1,72 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { AbstractControl, FormGroup } from '@angular/forms';
-import { creditCardForm } from 'src/config/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-credit-card-form',
   templateUrl: './credit-card-form.component.html',
-  styleUrls: ['./../forms-shared-css/forms.css'],
+  styleUrls: ['./credit-card-form.component.css'],
 })
-export class CreditCardFormComponent {
-  @Input() formGroup: FormGroup;
-  cardForm: any = creditCardForm;
-  today: Date = new Date();
-  currentMonth = this.today.getMonth() + 1;
-  currentYear = this.today.getFullYear();
-  constructor() {
-    this.populateOptionsAndSetDefaults();
-  }
-
-  private createMonthOptions() {
-    let monthOptions = [];
-    for (let i = 1; i <= 12; i++) {
-      monthOptions.push({
-        key: i,
-        value: i,
-      });
-    }
-
-    return monthOptions;
-  }
-
-  private populateOptionsAndSetDefaults() {
-    this.cardForm[2].options = this.createMonthOptions();
-    this.cardForm[2].value = this.currentMonth;
-    this.cardForm[3].options = this.createYearOptions();
-    this.cardForm[3].value = this.currentYear;
-  }
-
-  private createYearOptions() {
-    let yearOptions = [];
-    for (let i = this.currentYear; i <= this.currentYear + 10; i++) {
-      yearOptions.push({
-        key: i,
-        value: i,
-      });
-    }
-    return yearOptions;
-  }
-
-  ngOnInit(): void {
-    this.formGroup?.valueChanges.subscribe((value) => {
-      if (value.creditCard != null) {
-        this.limitFieldInput(this.cvvField!, 3);
-        this.limitFieldInput(this.cardNumberField!, 16);
-      }
-    });
-  }
-
-  private limitFieldInput(field: AbstractControl, limit: number) {
-    if (field.value.length > limit) {
-      field.setValue(field.value.slice(0, -1));
-    }
-  }
-
-  get cvvField(): AbstractControl {
-    return this.formGroup.get('creditCard')?.get('cvv')!;
-  }
-
-  get cardNumberField(): AbstractControl {
-    return this.formGroup.get('creditCard')?.get('cardNumber')!;
-  }
-}
+export class CreditCardFormComponent {}
