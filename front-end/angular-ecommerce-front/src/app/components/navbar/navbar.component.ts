107cf3ccc21d99987e0f77ce2eec90e9dac8c8b0
Alexander <gallade2014@gmail.com>
2023-09-09 21:19:09 -0600

    Reworked navbar component with a new style, and made it responsive.

--- front-end/angular-ecommerce-front/src/app/components/navbar/navbar.component.ts
+++ front-end/angular-ecommerce-front/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showMenu: boolean = false;
+  imageServerUrl: string = environment.imageServerUrl;
+
+  public toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+}
