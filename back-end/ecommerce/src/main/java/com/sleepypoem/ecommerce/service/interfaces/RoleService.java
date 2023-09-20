7b49c7f36b315ba9af4289705a2c679b795677f6
Alexander <gallade2014@gmail.com>
2023-09-20 10:50:21 -0600

    Created a role service interface.

---
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/service/interfaces/RoleService.java
@@ -0,0 +1,29 @@
+package com.sleepypoem.ecommerce.service.interfaces;
+
+import com.sleepypoem.ecommerce.domain.entities.PermissionEntity;
+import com.sleepypoem.ecommerce.domain.entities.RoleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RoleService {
+
+    RoleEntity save(RoleEntity role);
+
+    RoleEntity findById(Long id);
+
+    RoleEntity update(Long id, RoleEntity role);
+
+    void deleteById(Long id);
+
+    void delete(RoleEntity role);
+
+    Page<RoleEntity> findAll(Pageable pageable);
+
+    Page<RoleEntity> findByNameContaining(String name, Pageable pageable);
+
+    RoleEntity findByName(String name);
+
+    RoleEntity addPermission(Long roleId, PermissionEntity permission);
+
+    RoleEntity removePermission(Long roleId, Long permissionId);
+}
