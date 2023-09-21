20df44003bf1a484af762adad635d9a23093e08c
Alexander <gallade2014@gmail.com>
2023-09-21 14:56:37 -0600

    Added exception handlers for custom exceptions.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/GlobalExceptionHandler.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/config/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.sleepypoem.ecommerce.config;
 
 import com.okta.sdk.resource.client.ApiException;
+import com.sleepypoem.ecommerce.exceptions.MyBadRequestException;
+import com.sleepypoem.ecommerce.exceptions.MyEntityNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -18,5 +20,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return handleExceptionInternal(e, e.getResponseBody(), headers, HttpStatusCode.valueOf(e.getCode()), request);
+    }
+
+    @ExceptionHandler(MyEntityNotFoundException.class)
+    public ResponseEntity<Object> handleMyEntityNotFoundException(MyEntityNotFoundException e, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        String message = "{ \"error_message\": \"" + e.getMessage() + "\" }";
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return handleExceptionInternal(e, message, headers, HttpStatusCode.valueOf(404), request);
+    }
+
+    @ExceptionHandler(MyBadRequestException.class)
+    public ResponseEntity<Object> handleMyBadRequestException(MyBadRequestException e, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        String message = "{ \"error_message\": \"" + e.getMessage() + "\" }";
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return handleExceptionInternal(e, message, headers, HttpStatusCode.valueOf(400), request);
     }
 }
