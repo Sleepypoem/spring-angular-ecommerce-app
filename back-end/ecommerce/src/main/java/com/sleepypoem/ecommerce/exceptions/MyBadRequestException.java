ce6c3b03db9117aa762c24a495b1683675b9556f
Alexander <gallade2014@gmail.com>
2023-09-21 14:36:43 -0600

    Added a response status code for MyBadRequestException.

--- back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/exceptions/MyBadRequestException.java
+++ back-end/ecommerce/src/main/java/com/sleepypoem/ecommerce/exceptions/MyBadRequestException.java
@@ -1,5 +1,8 @@
 package com.sleepypoem.ecommerce.exceptions;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(org.springframework.http.HttpStatus.BAD_REQUEST)
 public class MyBadRequestException extends RuntimeException {
     public MyBadRequestException(String message) {
         super(message);
